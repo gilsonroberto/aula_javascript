54d514da87afd98708b617c658b2edf9409454a3
unknown <gilsonroberto@gmail.com>
2020-01-17 16:51:08 -0300

    FUNÇÕES

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
+/* FUNÇÕES */
+
+function soma(n1, n2){
+    return n1 + n2;
+};
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome);
+};
+
+function validaIdade(idade){
+    var validar;
+
+    if(idade >= 18){
+        validar = true;
+    }else{
+        validar =  false;
+    }
+    return validar;
+}
+
+var idade = prompt('Qual sua idade? ');
+console.log(validaIdade(idade));
+
+
+//alert(soma(5,10));
+//alert(setReplace('Vai Japão', 'Japão', 'Brasil'));
+
 /* CONDICIONAIS, LAÇOS DE REPETIÇAO E DATE */
 
 /* DATE */
-
+/*
 var d = new Date();
 alert(d);
 alert(d.getMonth()+1);
 alert(d.getMinutes());
 alert(d.getDay());
 alert(d.getHours());
-
+*/
 /* FOR */
 
 /*var count;
